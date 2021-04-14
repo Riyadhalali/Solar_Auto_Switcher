e26f45f8b9fcd56301233ba49283eda687d9d1de
Riyad <eng.riyadalali93@gmail.com>
2021-04-14 12:53:35 +0300

    1- Created Class for Connection 2- created buttons and edit text for handling the input data by user 3- created event listner  for the buton to connect to esp

---
+++ app/src/main/java/com/example/solarautoswitcher/Connection.java
@@ -0,0 +1,17 @@
+package com.example.solarautoswitcher;
+
+
+import android.os.AsyncTask;
+
+// this class is for the connection for the esp12e and how to send data to esp8266-12e
+public class Connection extends AsyncTask <String,Void,String>
+{
+
+    public Connection(String message) {
+    }
+
+    @Override
+    protected String doInBackground(String... strings) {
+        return null;
+    }
+}
--- app/src/main/java/com/example/solarautoswitcher/MainActivity.java
+++ app/src/main/java/com/example/solarautoswitcher/MainActivity.java
@@ -1,14 +1,54 @@
 package com.example.solarautoswitcher;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.content.AsyncTaskLoader;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.net.Socket;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnConfig;
+    EditText ssd,password;
+    Socket socket;
+    int port=80;
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+        btnConfig=(Button)findViewById(R.id.btnConfig);
+        ssd=(EditText)findViewById(R.id.ssd);
+        password=(EditText)findViewById(R.id.password);
+
+//----------------------------Connection Button----------------------------------------------------
+        btnConfig.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                String WifiSSid,WifiPassword,message;
+
+                WifiSSid=ssd.getText().toString(); // get the username
+                WifiPassword=password.getText().toString(); // get the password
+
+                message="#"+WifiSSid+"$"+WifiPassword+"@"; // decode the message for sending it to Esp
+
+                Connection taskPost=new Connection(message);//send this message
+                taskPost.execute("hello"); //do nothing just for not sending null object because app will crush
+                
+            }
+        });
+
+    } // onCreate
+//--------------------------------------------------------------------------------------------------
+
+
+} // end class
